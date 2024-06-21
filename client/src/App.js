ec08a522f992bd46ab121ee45e08d54dc4d7a1bc
sujit <sujithalder733124@gmail.com>
2024-06-21 21:42:04 +0530

    ja khusi

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
-
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Movielist from './Pages/Data/Movielist';
 import Slider from './Pages/Home/Components/Slider/Slider';
 import Movie from './Pages/Movie/Movie';
 import Home from './Pages/Home/Home';
-
-
-
+import AboutUs from './Pages/AboutUs/AboutUs';
+ 
 function App() {
-  const movie= Movielist[0]
+  const movie = Movielist[0]
   return (
     <div className="w-screen h-screen">
      <Router>
@@ -17,6 +16,7 @@ function App() {
         <Routes>
             <Route path="/" element= {<Home/>}></Route>
             <Route path="/movie/:movie_id" element= {<Movie/>}></Route>
+            <Route path="/AboutUs" element={<AboutUs/>} />
         </Routes>
 
      </Router>
